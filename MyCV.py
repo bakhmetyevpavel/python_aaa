4104a578b24d14960b911e3a6ffe7647b44d56e8
bakhmetyevpavel <47784037+bakhmetyevpavel@users.noreply.github.com>
2020-10-27 17:23:21 +0300

    Add files via upload
    My HW3 task. My CountVectorizer.

---
+++ MyCV.py
@@ -0,0 +1,76 @@
+import re
+from typing import List
+
+
+class MyCountVectorizer:
+    """
+    My CountVectorizer MVP
+    """
+
+    def __init__(self):
+        self._prepared_corpus = []
+        self._bag_of_words = []
+        self._count_matrix = []
+
+    def get_feature_names(self):
+        """
+        Returns a list of feature names.
+        """
+        return self._bag_of_words
+
+    def _corpus_preprocessing(self, corpus: List[str]):
+        """
+        Input text preprocessing
+        """
+        for index in range(len(corpus)):
+            corpus[index] = corpus[index].lower()
+            corpus[index] = corpus[index].strip()
+            corpus[index] = re.sub(r" +", " ", corpus[index])
+            corpus[index] = re.sub(r"[^A-za-zА-Яа-я- ]+", "", corpus[index])
+
+        self._prepared_corpus = corpus
+
+    def _tokenize_corpus(self):
+        """
+        Corpus tokenization
+        """
+        for string in self._prepared_corpus:
+            for item in string.split():
+                if item not in self._bag_of_words:
+                    self._bag_of_words.append(item)
+
+    def _get_matrix(self):
+        """
+        Returns result matrix as list of lists
+        """
+        for string in self._prepared_corpus:
+            self._count_matrix.append(
+                [string.split().count(word) for word in self._bag_of_words]
+            )
+
+    def fit_transform(self, corpus: List[str]) -> List[List[int]]:
+        """
+        Learn the vocabulary dictionary and return document-term matrix.
+
+        Parameters:
+        -----------
+
+        corpus: list
+            Raw document as a list of sentenses
+        """
+        self._corpus_preprocessing(corpus)
+        self._tokenize_corpus()
+        self._get_matrix()
+        return self._count_matrix
+
+
+if __name__ == "__main__":
+    corpus = [
+        "  Crock Pot Pa@@sta Never boil       pasta again",
+        "Pasta Pomodoro Fresh   213 ingredients Parmesan to taste",
+    ]
+
+    vectorizer = MyCountVectorizer()
+    matrix = vectorizer.fit_transform(corpus)
+    print(vectorizer.get_feature_names())
+    print(matrix)
